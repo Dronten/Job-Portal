990161bd6707d51066b26a8daad7c9321651aca5
Dronten <drontengaming2004@gmail.com>
2026-08-07 23:19:32 +0530

    Add code for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,23 +7,28 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
+// connect to database
+connectDB();
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corseOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials: true
 }
 
 app.use(cors(corseOptions));
 
-const PORT = process.env.PORT ||3000;
 
 // routes
 app.use("/api/v1/user", userRoute);
@@ -31,8 +36,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get((_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
-app.listen(PORT,() =>{
-    connectDB();
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
