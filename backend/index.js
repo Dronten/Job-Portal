f93e250978c6e218d4f8e353201c5883b1a9cc10
Dronten <drontengaming2004@gmail.com>
2026-08-08 22:29:02 +0530

    fix wildcard route path and CORS origin for production

--- backend/index.js
+++ backend/index.js
@@ -22,12 +22,12 @@ const _dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corseOptions = {
-    origin: 'http://localhost:5173',
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://job-portal-rq4c.onrender.com'],
     credentials: true
 }
 
-app.use(cors(corseOptions));
+app.use(cors(corsOptions));
 
 
 // routes
@@ -37,7 +37,7 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get((_, res) => {
+app.get("/*splat", (_, res) => {
     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 })
 
